5adf30a4e53c6422b3a3144bcb03465e542b1e19
Alexander Predeus <predeus@gmail.com>
2018-09-13 17:15:50 +0100

    Make big tables

--- unify_exp_tables.pl
+++ unify_exp_tables.pl
@@ -4,186 +4,192 @@ use strict;
 use warnings; 
 use Data::Dumper; 
 
-## make master expression table with Roary ortholog table 
-## this also uses phages annotation from PHAST and GFF files 
-## to annotate genes as belonging to chromosome, prophage, or a plasmid
+## take annotated tables of CDS and ncRNA 
+## and expression tables of all individual strains 
+## and get the master expression table 
+## gene names that have a non - group_* name with underscore (e.g. argC_1) 
+## are stripped or the _* part 
+## non-unique genes are then assigned new unique numbers (e.g. 2 x argC becomes argC_1, argC_2)
+## group_* genes are replaced with STM* whenever possible. 
 
-my $map = shift @ARGV; 
-my $bed1 = shift @ARGV; 
-my $bed2 = shift @ARGV; 
-my $gff1 = shift @ARGV; 
-my $gff2 = shift @ARGV; 
-my $expmat1 = shift @ARGV; ## assume bacpipe format - 3-col annotation for genes
-my $expmat2 = shift @ARGV; 
 
-my $phage1={}; 
-my $phage2={};
-my $loc1={}; 
-my $loc2={};
-my $exp1={};
-my $exp2={};
+my $wdir = shift @ARGV; 
+my $config = shift @ARGV; 
+my $ann_cds = shift @ARGV; 
+my $ann_nc = shift @ARGV;
+my $ref_strain = shift @ARGV;  
 
-my $chr_name1="D23580_liv_chro";  # I'll cheat here  
-my $chr_name2="CP002487.fasta"; 
+my $sample_counts = {};
+my $strain_idx = {};  
+my $ann = {}; 
+my $counts = {};
+my $tpms = {}; 
+my @ref_strains;  
 
-open  MAP,"<",$map  or die "$!";   
-open BED1,"<",$bed1 or die "$!";   
-open BED2,"<",$bed2 or die "$!";   
-open GFF1,"<",$gff1 or die "$!";   
-open GFF2,"<",$gff2 or die "$!";   
-open EXP1,"<",$expmat1 or die "$!";   
-open EXP2,"<",$expmat2 or die "$!";   
+open CONFIG,"<",$config or die "$!"; 
+open ANN_CDS,"<",$ann_cds or die "$!"; 
+open ANN_NC,"<",$ann_nc or die "$!"; 
 
-while (<BED1>) { 
-  chomp; 
-  my @tt = split /\t+/; 
-  $phage1->{$tt[3]}->{chr}=$tt[0];
-  $phage1->{$tt[3]}->{begin}=$tt[1];
-  $phage1->{$tt[3]}->{end}=$tt[2];
-} 
-
-while (<BED2>) { 
-  chomp; 
-  my @tt = split /\t+/; 
-  $phage2->{$tt[3]}->{chr}=$tt[0];
-  $phage2->{$tt[3]}->{begin}=$tt[1];
-  $phage2->{$tt[3]}->{end}=$tt[2];
-}
-
-while (<GFF1>) { 
-  chomp; 
-  my @tt = split /\t+/;
-  $tt[8] =~ m/ID=(.*?);.*/;
-  my $id = $1; 
-  $tt[8] =~ m/.*;Name=(.*?);.*/;
-  my $name = ($1 ne "") ? $1 : "NONE"; 
-  $loc1->{$id}->{chr}=$tt[0]; 
-  $loc1->{$id}->{begin}=$tt[3]-1;  ## we're doing all 0-based  
-  $loc1->{$id}->{end}=$tt[4]; 
-  $loc1->{$id}->{name}=$name;
-} 
-
-while (<GFF2>) { 
-  chomp; 
-  my @tt = split /\t+/; 
-  $tt[8] =~ m/ID=(.*?);.*/;
-  my $id = $1; 
-  $tt[8] =~ m/.*;Name=(.*?);.*/;
-  my $name = ($1 ne "") ? $1 : "NONE"; 
-  $loc2->{$id}->{chr}=$tt[0]; 
-  $loc2->{$id}->{begin}=$tt[3]-1;  ## we're doing all 0-based  
-  $loc2->{$id}->{end}=$tt[4]; 
-  $loc2->{$id}->{name}=$name;
-} 
-
-my $h1 = <EXP1>; 
-chomp $h1; 
-my $h2 = <EXP2>; 
-chomp $h2; 
-my @h1 = split /\t+/, $h1;
-my @h2 = split /\t+/, $h2;
-my $nsmp1 = scalar @h1 - 1;        ## how many samples in matrix 1
-my $nsmp2 = scalar @h2 - 1;        ## same for exp. matrix 2
-$h1 = join "\t",@h1[3..$nsmp1]; 
-$h2 = join "\t",@h2[3..$nsmp2]; 
-
-while (<EXP1>) { 
-  chomp; 
-  my @tt = split /\t+/;
-  my $id = shift @tt; 
-  my $name = shift @tt;  
-  shift @tt; 
-  my $exp = join "\t",@tt; 
-  $exp1->{$id}->{name}=$name; 
-  $exp1->{$id}->{exp}=$exp; 
-}
-
-while (<EXP2>) { 
-  chomp; 
-  my @tt = split /\t+/;
-  my $id = shift @tt; 
-  my $name = shift @tt;  
-  shift @tt; 
-  my $exp = join "\t",@tt; 
-  $exp2->{$id}->{name}=$name; 
-  $exp2->{$id}->{exp}=$exp; 
-}
- 
-## let's do it finally! 
-## assume that map has format name-(tab)-id1-(tab)-id2
-
-print "D23_id\tD23_anno\tL474_id\tL474_anno\tGene_name\t$h1\t$h2\n"; 
-
-while (<MAP>) { 
+## parse config, get list of study strains
+## get how many samples are there for each strain 
+while (<CONFIG>) { 
   chomp;
-  my $ann1=""; 
-  my $ann2=""; 
-  my $expvec1=""; 
-  my $expvec2=""; 
-  my $id1 = (split /\t+/)[0]; 
-  my $id2 = (split /\t+/)[1];
-  my $names = (split /\t+/)[2]; 
- 
-  if ($id1 ne "NONE") { 
-    my $chr1 = $loc1->{$id1}->{chr}; 
-    my $beg1 = $loc1->{$id1}->{begin}; 
-    my $end1 = $loc1->{$id1}->{end}; 
-    if ($chr1 eq $chr_name1) { 
-      $ann1 = "chrom"; 
-      foreach my $phid (keys %{$phage1}) { 
-	my $pb1 = $phage1->{$phid}->{begin}; 
-        my $pe1 = $phage1->{$phid}->{end}; 
-        $ann1 = "prophage" if (($beg1 >= $pb1 && $beg1 <= $pe1) || ($end1 >= $pb1 && $end1 <= $pb1));
-      } 
-    } else { 
-      $ann1 = "plasmid"; 
-    }
-  } else { 
-    $ann1 = "NONE"; 
-  }
-  
-  if ($id2 ne "NONE") { 
-    my $chr2 = $loc2->{$id2}->{chr}; 
-    my $beg2 = $loc2->{$id2}->{begin}; 
-    my $end2 = $loc2->{$id2}->{end}; 
-    if ($chr2 eq $chr_name2) { 
-      $ann2 = "chrom"; 
-      foreach my $phid (keys %{$phage2}) { 
-	my $pb2 = $phage2->{$phid}->{begin}; 
-        my $pe2 = $phage2->{$phid}->{end}; 
-        $ann2 = "prophage" if (($beg2 >= $pb2 && $beg2 <= $pe2) || ($end2 >= $pb2 && $end2 <= $pb2));
-      } 
-    } else { 
-      $ann2 = "plasmid"; 
-    }
-  } else { 
-    $ann2 = "NONE"; 
-  }
-  
-  if ($id1 ne "NONE") {
-    $expvec1 = $exp1->{$id1}->{exp};
-  } else { 
-    my $veclen=$nsmp1-2;
-    my @zero = ("0") x $veclen; 
-    $expvec1 = join "\t",@zero;
-  }
-  
-  if ($id2 ne "NONE") {
-    $expvec2 = $exp2->{$id2}->{exp};
-  } else {
-    my $veclen=$nsmp2-2;
-    my @zero = ("0") x $veclen; 
-    $expvec2 = join "\t",@zero;
+  my @tt = split /\t+/;  
+  my $strain = $tt[1]; 
+  if (! m/^Reference/ && defined $sample_counts->{$strain}) {
+    $sample_counts->{$strain}++; 
+  } elsif (! m/^Reference/ && ! defined $sample_counts->{$strain}) { 
+    $sample_counts->{$strain}=1; 
+  } elsif (m/^Reference/) {
+    ## we assume that each ref strain is only listed once 
+    push @ref_strains,$tt[1];
   } 
-  
-  printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$id1,$ann1,$id2,$ann2,$names,$expvec1,$expvec2;  
-} 
-  
+}
 
-close BED1;
-close BED2;
-close GFF1;
-close GFF2;
-close EXP1;
-close EXP2; 
-close MAP; 
+my @study_strains = keys %{$sample_counts}; 
+@study_strains = sort { $a cmp $b } @study_strains;
+@ref_strains   = sort { $a cmp $b } @ref_strains;
+my @strains = @study_strains; 
+push @strains,@ref_strains; 
+
+my $ref_index; 
+#print Dumper($sample_counts); 
+
+## parse annotation files, populate hash
+my $cds_header = <ANN_CDS>; 
+chomp $cds_header; 
+my $nc_header = <ANN_NC>; 
+chomp $nc_header; 
+my @str_names = split /\t+/,$cds_header;
+
+$cds_header =~ s/Gene_name\tGene_loc\t//g;  
+$nc_header  =~ s/ncRNA_name\tncRNA_loc\t//g;  
+die "ERROR: CDS and ncRNA annotations have different strain order!\n" if ($cds_header ne $nc_header); 
+
+## get the column numbers 
+for (my $i=0; $i < scalar @str_names; $i++) { 
+  foreach my $strain (@strains) {
+    $strain_idx->{$strain} = $i-2 if ($str_names[$i] eq $strain);
+  }
+} 
+
+#print Dumper($strain_idx); 
+
+## populate annotation hash 
+while (<ANN_CDS>) { 
+  chomp;
+  my $name_count = 2;  
+  my @tt   = split /\t+/;
+  my $name = shift @tt; 
+  my $loc  = shift @tt;
+  my $ref_idx = $strain_idx->{$ref_strain};
+  #print "DEBUG1: $ref_strain $ref_idx\n";  
+  my $ref_lt = $tt[$ref_idx]; 
+  
+  $name =~ s/_*//g if ($name !~ m/^group_/);
+  $name = $ref_lt if ($name =~ m/^group_/ && $ref_lt ne "NONE"); 
+  
+  if (! defined $ann->{$name}) { 
+    foreach my $strain (@strains) { 
+      my $idx = $strain_idx->{$strain}; 
+      my $locus_tag = $tt[$idx];                    ## account for two shifts above  
+      $ann->{$name}->{$strain}->{lt} = $locus_tag;
+      $ann->{$name}->{loc} = $loc;                    ## chr/prophage/plasmid  
+    } 
+  } else { 
+    $name = join "_",$name,$name_count; 
+    $name_count++; 
+    foreach my $strain (@strains) {
+      my $idx = $strain_idx->{$strain};
+      my $locus_tag = $tt[$idx];                    ## account for two shifts above  
+      $ann->{$name}->{$strain}->{lt} = $locus_tag;
+      $ann->{$name}->{loc} = $loc;                    ## chr/prophage/plasmid  
+    }
+  }
+}
+
+while (<ANN_NC>) { 
+  chomp;
+  my $name_count = 2;  
+  my @tt   = split /\t+/;
+  my $name = shift @tt; 
+  my $loc  = shift @tt;
+  $name    =~ s/_*//g if ($name !~ m/^group_/); 
+  
+  if (! defined $ann->{$name}) { 
+    foreach my $strain (@strains) { 
+      my $idx = $strain_idx->{$strain}; 
+      my $locus_tag = $tt[$idx];                    ## account for two shifts above  
+      $ann->{$name}->{$strain}->{lt} = $locus_tag;
+      $ann->{$name}->{loc} = $loc;                  ## chr/prophage/plasmid  
+    } 
+  } else { 
+    $name = join "_",$name,$name_count; 
+    $name_count++; 
+    foreach my $strain (@strains) {
+      my $idx = $strain_idx->{$strain};
+      my $locus_tag = $tt[$idx];                    ## account for two shifts above  
+      $ann->{$name}->{$strain}->{lt} = $locus_tag;
+      $ann->{$name}->{loc} = $loc;                  ## chr/prophage/plasmid  
+    }
+  }
+}
+
+#print Dumper($ann); 
+
+## finally, let's print the full annotated table
+## for this, we first populate the counts hash
+foreach my $strain (@study_strains) { 
+  my $counts_file = join "/",$wdir,"exp_tables",$strain; 
+  my $tpm_file = join "/",$wdir,"exp_tables",$strain; 
+  $counts_file = join ".",$counts_file,"counts.tsv";
+  $tpm_file = join ".",$tpm_file,"TPM.tsv";
+  open COUNTS,"<",$counts_file or die "$!";  
+  open TPMS,"<",$tpm_file or die "$!";  
+  
+  my $samples = <TPMS>; 
+  $samples = <COUNTS>;
+  $samples =~ s/$strain\t//g; 
+  $counts->{$strain}->{samples}=$samples; 
+  $tpms->{$strain}->{samples}=$samples; 
+  
+  while (<COUNTS>) { 
+    chomp; 
+    m/^(.*?)\t(.*)/;
+    my $locus_tag = $1;
+    my $count_str = $2; 
+    $counts->{$strain}->{$locus_tag}=$count_str; 
+  } 
+  while (<TPMS>) { 
+    chomp; 
+    m/^(.*?)\t(.*)/;
+    my $locus_tag = $1;
+    my $tpm_str = $2; 
+    $tpms->{$strain}->{$locus_tag}=$tpm_str; 
+  }
+  
+  close COUNTS; 
+  close TPMS; 
+}
+
+#print Dumper($tpms);   
+
+## and now, the actual table. Serously, fuck this thing sideways. 
+foreach my $name (keys %{$ann}) { 
+  my $flag = 1; 
+  my $output = join "\t",$name,$ann->{$name}->{loc};
+  foreach my $strain (@strains) { 
+    $output = join "\t",$output,$ann->{$name}->{$strain}->{lt};
+  }
+  foreach my $strain (@study_strains) { 
+    my $locus_tag = $ann->{$name}->{$strain}->{lt}; 
+    my $counts = $counts->{$strain}->{$locus_tag}; 
+    $output = join "\t",$output,$counts; 
+    #my $tpms = $tpms->{$strain}->{$locus_tag}; 
+  } 
+  print "$output\n"; 
+}
+
+close CONFIG; 
+close ANN_CDS; 
+close ANN_NC; 
